8e287e6c00b0d181e2042c6182663ff5e8fb191f
Juan Manuel Santa Cruz <juanmasantax@gmail.com>
2026-03-02 19:52:46 -0300

    FIX: folders, dependences

---
+++ Santax-PP/src/main/java/com/taller/backend/SantaxPpApplication.java
@@ -0,0 +1,13 @@
+package com.taller.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SantaxPpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SantaxPpApplication.class, args);
+	}
+
+}
